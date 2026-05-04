9c216d29df61e97b712f0384388738647104730e
Bane Sullivan <22067021+banesullivan@users.noreply.github.com>
2026-05-04 08:44:57 -0700

    Use dynamic version

--- src/pyvista_stl/__init__.py
+++ src/pyvista_stl/__init__.py
@@ -1,13 +1,11 @@
 """pyvista-stl reader library."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 from pyvista_stl.reader import read, read_as_mesh
 
 try:
-    __version__ = version("pyvista-stl")
-except PackageNotFoundError:
-    __version__ = "unknown"
+    from pyvista_stl._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
 
 
 __all__ = ["read", "read_as_mesh", "__version__"]
